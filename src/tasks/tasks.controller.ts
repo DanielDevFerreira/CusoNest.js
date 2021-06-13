f5084c0fed99059b78cd3293795b711a03861b5e
DanielDevFerreira <danielplay78@hotmail.com>
2021-06-13 20:55:15 -0300

    End of the first section of curso nest.js

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { Task } from './tasks.model';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 
@@ -9,18 +11,52 @@ export class TasksController {
     constructor (private taskService: TasksService){}
 
     @Get()
-    getAllTasks(): Task[]{
-        return this.taskService.getAllTasks();
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[]{
+    // if we have any filters defined, call tasksService.getTasksWithFilters
+    // otherwise, just get all tasks
+        if(Object.keys(filterDto).length){
+            return this.taskService.getTaskWithFilters(filterDto);
+        }else {
+            return this.taskService.getAllTasks();
+        }     
     }
 
+    // @Get()
+    // getAllTasks(): Task[]{
+    //     return this.taskService.getAllTasks();
+    // }
+
+    // @Post()
+    // // evitar de passar atributo errado, forçando o tipo no Body
+    // createTask(@Body('title') title: string, @Body('description') description: string){
+    //     return this.taskService.createTask(title, description);
+    // }
+
+    // Mesmo método, porem usando DTO
     @Post()
-    createTask(
-        // evitar de passar atributo errado, forçando o tipo no Body
-        @Body('title') title: string,
-        @Body('description') description: string
-    ){
-        console.log('title', title);
-        console.log('description', description);
+    createTask(@Body() createTaskDto: CreateTaskDto){
+        return this.taskService.createTask(createTaskDto);
     }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
+    @Delete('/:id')
+    removeTaskById(@Param('id') id: string): void{
+        return this.taskService.removeTaskById(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task{
+        return this.taskService.updateTaskStatus(id, status);
+    }
+
+    @Patch('/:id/description')
+    updateTaskDescription(@Param('id') id: string, @Body('description') description: string): Task{
+        return this.taskService.updateTaskDescriptio(id, description);
+    }
+
 }
  
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +12,45 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task{
+    getTaskWithFilters(filterDto: GetTasksFilterDto){
+        const { status, search } = filterDto;
+
+        // define a temporary array to hold the result
+        let tasks = this.getAllTasks();
+        // do something with status
+        if(status){
+            tasks = tasks.filter((task) => task.status === status);
+        }
+        // do something with search
+        if(search){
+            tasks = tasks.filter((task) => {
+                if(task.title.toLowerCase().includes(search) || task.description.toLowerCase().includes(search)){
+                    return true;
+                }else {
+                    return false;
+                }
+            })
+        }
+        // return final result
+        return tasks;
+    }
+
+    // createTask(title: string, description: string): Task{
+    //     const task: Task = {
+    //         id: uuid(),
+    //         title,
+    //         description,
+    //         status: TaskStatus.OPEN
+    //     };
+
+    //     this.tasks.push(task);
+    //     return task;
+    // }
+
+    // o mesmo método, usando DTO
+    createTask(createTaskDto: CreateTaskDto): Task{
+        const {title, description } = createTaskDto;
+
         const task: Task = {
             id: uuid(),
             title,
@@ -19,6 +59,26 @@ export class TasksService {
         };
 
         this.tasks.push(task);
+        return task;
+    }
+
+    getTaskById(id: string): Task{
+        return this.tasks.find((task) => task.id === id)
+    }
+
+    removeTaskById(id: string): void{
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus){
+        const task = this.getTaskById(id);
+        task.status = status;
+        return task;
+    }
+
+    updateTaskDescriptio(id: string, description: string){
+        const task = this.getTaskById(id);
+        task.description = description;
         return task;
     }
 }
